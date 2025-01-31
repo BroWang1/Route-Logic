1e8c24120fa76e457d40512d798541843112d72f
Brodey Wang <135397500+BroWang1@users.noreply.github.com>
2025-01-31 10:21:25 -0500

    Create About.js

---
+++ client/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Route Logic</h1>
+      <p>We specialize in cutting-edge technology to solve complex problems.</p>
+    </div>
+  );
+};
+
+export default About;
