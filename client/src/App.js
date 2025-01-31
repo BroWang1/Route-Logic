bc36d893dd58cd3a84b20444c4227863ba79ca1a
Brodey Wang <135397500+BroWang1@users.noreply.github.com>
2025-01-31 14:07:33 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Route } from 'react-router-dom'; // Import routing components
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Correct imports
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -8,22 +8,22 @@ import BusinessPlan from './components/BusinessPlan';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
       <div style={{ textAlign: 'center' }}>
         {/* Logo */}
         <img src="/logo.png" alt="RouteLogic Logo" />
         <h1>Welcome to RouteLogic</h1>
         
-        {/* Routes for different components */}
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/business-plan" component={BusinessPlan} />
-        </Switch>
+        {/* Routes for different components (React Router v6+) */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/business-plan" element={<BusinessPlan />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
