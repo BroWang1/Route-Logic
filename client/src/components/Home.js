8cc23528d4a1b34c37bfd23cc7753d38fb1408e0
Brodey Wang <135397500+BroWang1@users.noreply.github.com>
2025-01-31 10:21:01 -0500

    Create Home.js

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Route Logic</h1>
+      <p>Innovative solutions for a smarter future.</p>
+    </div>
+  );
+};
+
+export default Home;
