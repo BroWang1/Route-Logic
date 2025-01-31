451b6b8e4fa3b781997e9bd0c51702a3394eeed1
Brodey Wang <135397500+BroWang1@users.noreply.github.com>
2025-01-30 19:32:42 -0500

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import BusinessPlan from './components/BusinessPlan';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/business-plan" element={<BusinessPlan />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
