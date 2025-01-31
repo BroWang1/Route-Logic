8f7731ef81adca44617be08e23d50b93bbcef681
Brodey Wang <135397500+BroWang1@users.noreply.github.com>
2025-01-31 14:04:42 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'; // Import routing components
+import { Route, Route } from 'react-router-dom'; // Import routing components
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
