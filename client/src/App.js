c63a4f696b3e4ba79b043dd5b91c7b7de8086373
Brodey Wang <135397500+BroWang1@users.noreply.github.com>
2025-02-01 09:34:22 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <div style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ textAlign: 'center' }}>
         <img src="/logo.png" alt="RouteLogic Logo" className="w-64 h-auto" />
         {/* <h1>Welcome to RouteLogic</h1> */}
         
