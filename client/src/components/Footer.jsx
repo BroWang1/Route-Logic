7130675bbe81d668a87c2b690daa87c44e4b8182
Brodey Wang <135397500+BroWang1@users.noreply.github.com>
2025-02-10 17:14:55 -0500

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-black text-white py-12 w-full">
+    <footer className="bg-black text-white py-12 w-screen">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-sm px-6">
         
         {/* Our Research */}
