c7bbd0f7042d68ada181fd484958362052138a68
Brodey Wang <135397500+BroWang1@users.noreply.github.com>
2025-02-10 09:47:23 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,30 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import InvestorRelations from './components/InvestorRelations';
+import Footer from './components/Footer'; // Import Footer
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <img src="/logo.png" alt="RouteLogic Logo" className="w-64 h-auto" />
-        {/* <h1>Welcome to RouteLogic</h1> */}
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
         
-        {/* Routes for different components (React Router v6+) */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/investor-relations" element={<InvestorRelations />} />
-        </Routes>
+        {/* Main content area */}
+        <div className="flex-grow flex flex-col items-center justify-center">
+          <img src="/logo.png" alt="RouteLogic Logo" className="w-64 h-auto" />
+          {/* <h1>Welcome to RouteLogic</h1> */}
+
+          {/* Routes for different components (React Router v6+) */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/investor-relations" element={<InvestorRelations />} />
+          </Routes>
+        </div>
+
+        {/* Footer at the bottom */}
+        <Footer />
       </div>
     </Router>
   );
